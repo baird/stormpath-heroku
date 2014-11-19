8df3a704f8bf56d4b18c83f0c478f5d1e6a1d523
Randall Degges <r@rdegges.com>
2014-11-19 14:32:27 -0800

    b

--- routes/index.js
+++ routes/index.js
@@ -3,32 +3,32 @@ var router = express.Router();
 
 
 // Render the home page.
-router.get('/', function(req, res) {
-  res.render('index', {title: 'Home', user: req.user});
-});
-
-
-// Render the dashboard page.
-router.get('/dashboard', function (req, res) {
-  if (!req.user || req.user.status !== 'ENABLED') {
-    return res.redirect('/login');
-  }
-
-  res.render('dashboard', {title: 'Dashboard', user: req.user});
-});
-
-// Render the settings page.
-router.get('/settings', function (req, res) {
-  if (!req.user || req.user.status !== 'ENABLED') {
-    return res.redirect('/login');
-  }
-
-  res.render('settings', {title: 'settings', user: req.user});
-});
-
-////TESTS////
-
-
+//router.get('/', function(req, res) {
+//  res.render('index', {title: 'Home', user: req.user});
+//});
+//
+//
+//// Render the dashboard page.
+//router.get('/dashboard', function (req, res) {
+//  if (!req.user || req.user.status !== 'ENABLED') {
+//    return res.redirect('/login');
+//  }
+//
+//  res.render('dashboard', {title: 'Dashboard', user: req.user});
+//});
+//
+//// Render the settings page.
+//router.get('/settings', function (req, res) {
+//  if (!req.user || req.user.status !== 'ENABLED') {
+//    return res.redirect('/login');
+//  }
+//
+//  res.render('settings', {title: 'settings', user: req.user});
+//});
+//
+//////TESTS////
+//
+//
 //// Token save page?
 //router.get('/savetoken', function (req, res) {
 //  if (!req.user || req.user.status !== 'ENABLED') {
